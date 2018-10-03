b71fd5e0558e64c43230c8ca9896818ef982cc97
lennart van laake <l.vanlaake@qquest.nl>
2018-10-03 13:57:39 +0200

    Memory leak proberen te fixen

--- sniff_exporter.py
+++ sniff_exporter.py
@@ -5,7 +5,6 @@ import random
 import time
 
 # Count packets
-ping_counter = Counter('count_icmp', 'total icmp calls - indicative of number of pings')
 traffic_counter = Counter('count_traffic', 'total calls to legit ports',  ['port'])
 nmap_counter = Counter('count_nmap', 'total calls to suspicious ports',  ['ip_city', 'port'])
 ssh_counter = Counter('count_ssh', 'total packets to ssh',  ['ip_city', 'port'])
@@ -23,22 +22,23 @@ def log_or_get_city(ip):
     return ip_city
 
 def count(p):
-    if p.dst == p.src:
-        return
-    if hasattr(p.getlayer(ICMP), 'type') and str(p.getlayer(ICMP).type) == "8":
-        ping_counter.inc()
-    elif IP in p and p[IP].proto == 6:
-        if hasattr(p, 'dport'):
-            if p.dport in [80, 443, 3000, 8080]:
-                traffic_counter.labels(port=p.dport).inc()
-            elif p.dport == 22:
-                ip = p[IP].src
-                ssh_counter.labels(ip_city=log_or_get_city(ip), port="22").inc()
-            elif p.dport < 1024:
-                ip = p[IP].src
-                nmap_counter.labels(ip_city=log_or_get_city(ip), port=p.dport).inc()
+    # if p.dst == p.src:
+    #     return
+    # if IP in p:
+    #     if hasattr(p, 'dport'):
+    try:
+        if p.dport in [80, 443, 3000, 8080]:
+            traffic_counter.labels(port=p.dport).inc()
+        elif p.dport == 22:
+            ip = p[IP].src
+            ssh_counter.labels(ip_city=log_or_get_city(ip), port="22").inc()
+        elif p.dport < 1024:
+            ip = p[IP].src
+            nmap_counter.labels(ip_city=log_or_get_city(ip), port=p.dport).inc()
+    except:
+        pass
 
 if __name__ == '__main__':
     start_http_server(6789)
-    sniff(prn=count)
+    sniff(prn=count, filter='tcp and (dst portrange 1-1023 or dst port 8080 or dst port 3000)', store=0)
 
