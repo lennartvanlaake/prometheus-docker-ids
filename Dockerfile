8493568b0477fc303add1f415f82e27ce9e4b9ad
lennart van laake <l.vanlaake@qquest.nl>
2018-10-03 13:15:53 +0200

    stad, ssh, verkeer en nmap trackers

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:2.7.15-alpine3.7
-RUN pip install scapy prometheus_client
+RUN pip install scapy prometheus_client requests
 ADD sniff_exporter.py /
 EXPOSE 8000
 CMD [ "python", "/sniff_exporter.py" ]
--- sniff_exporter.py
+++ sniff_exporter.py
@@ -1,16 +1,44 @@
-from scapy.all import sniff, IP
+from scapy.all import sniff, IP, ICMP
 from prometheus_client import start_http_server, Counter
+import requests
 import random
 import time
 
 # Count packets
-c = Counter('count', 'test', ['port', 'ip'])
+ping_counter = Counter('count_icmp', 'total icmp calls - indicative of number of pings')
+traffic_counter = Counter('count_traffic', 'total calls to legit ports',  ['port'])
+nmap_counter = Counter('count_nmap', 'total calls to suspicious ports',  ['ip_city', 'port'])
+ssh_counter = Counter('count_ssh', 'total packets to ssh',  ['ip_city', 'port'])
+ip_map = {}
+def log_or_get_city(ip):
+    if ip in ip_map:
+        ip_city = ip_map[ip]
+    else:
+        try:
+            ip_city = requests.get('http://ip-api.com/json/' + ip).json()['city']
+            ip_map[ip] = ip_city
+        except:
+            ip_city = "N/A"
+        ip_map[ip] = ip_city
+    return ip_city
 
 def count(p):
-    if hasattr(p, 'dport') and IP in p and hasattr(p[IP], 'dst'):
-        c.labels(port=p.dport, ip=p[IP].dst).inc()
+    if p.dst == p.src:
+        return
+    if hasattr(p.getlayer(ICMP), 'type') and str(p.getlayer(ICMP).type) == "8":
+        ping_counter.inc()
+    elif IP in p and p[IP].proto == 6:
+        if hasattr(p, 'dport'):
+            if p.dport in [80, 443, 3000, 8080]:
+                traffic_counter.labels(port=p.dport).inc()
+            elif p.dport == 22:
+                ip = p[IP].src
+                ssh_counter.labels(ip_city=log_or_get_city(ip), port="22").inc()
+            elif p.dport < 1024:
+                ip = p[IP].src
+                nmap_counter.labels(ip_city=log_or_get_city(ip), port=p.dport).inc()
 
 if __name__ == '__main__':
     start_http_server(8000)
-    sniff(prn=count, filter='tcp and !(dst host 192.168.1.103 or dst host 127.0.0.1)')
+    sniff(prn=count)
 
